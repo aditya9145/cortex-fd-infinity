13a797bafe9f829d9c2aeec54de3af81b3d19281
aditya9145 <25f2000030@ds.study.iitm.ac.in>
2026-03-04 00:57:50 +0530

    Add feature creation function for DataFrame

--- engines/feature_engine.py
+++ engines/feature_engine.py
@@ -1,1 +1,11 @@
+def create_features(df):
 
+    df["amount_zscore"] = (
+        (df["amount"] - df["amount"].mean()) /
+        (df["amount"].std() + 1)
+    )
+
+    df["night_flag"] = df["hour"].apply(lambda x: 1 if x < 5 else 0)
+    df["burst_index"] = 1 / (df["tx_gap"] + 1)
+
+    return df
